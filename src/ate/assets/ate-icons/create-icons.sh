c3efca19c87273de8a6d81676ec7cd9f7e318150
Mark Hobson <mark.hobson@activetravelengland.gov.uk>
2026-04-15 12:22:04 +0100

    Create ATE favicon.ico relative to repository directory

--- src/ate/assets/ate-icons/create-icons.sh
+++ src/ate/assets/ate-icons/create-icons.sh
@@ -3,19 +3,20 @@
 set -eo pipefail
 
 REPO_DIR=$(git rev-parse --show-toplevel)
+SOURCE_DIR="${REPO_DIR}/node_modules/govuk-frontend/dist/govuk/assets/images"
+TARGET_DIR="${REPO_DIR}/src/ate/assets/ate-icons"
 
 # favicon.svg
-cp "${REPO_DIR}/node_modules/govuk-frontend/dist/govuk/assets/images/favicon.svg" \
-	"${REPO_DIR}/src/ate/assets/ate-icons/favicon.svg"
-git apply ${REPO_DIR}/src/ate/assets/ate-icons/*.patch
+cp "${SOURCE_DIR}/favicon.svg" "${TARGET_DIR}/favicon.svg"
+git apply ${TARGET_DIR}/*.patch
 
 # favicon.ico
-inkscape -w 16 -h 16 -o favicon-16.png favicon.svg
-inkscape -w 32 -h 32 -o favicon-32.png favicon.svg
-inkscape -w 48 -h 48 -o favicon-48.png favicon.svg
+inkscape -w 16 -h 16 -o "${TARGET_DIR}/favicon-16.png" "${TARGET_DIR}/favicon.svg"
+inkscape -w 32 -h 32 -o "${TARGET_DIR}/favicon-32.png" "${TARGET_DIR}/favicon.svg"
+inkscape -w 48 -h 48 -o "${TARGET_DIR}/favicon-48.png" "${TARGET_DIR}/favicon.svg"
 convert \
-	\( favicon-16.png -colors 16 \) \
-	\( favicon-32.png -colors 32 \) \
-	\( favicon-48.png -colors 32 \) \
-	favicon.ico
-rm favicon-16.png favicon-32.png favicon-48.png
+	\( "${TARGET_DIR}/favicon-16.png" -colors 16 \) \
+	\( "${TARGET_DIR}/favicon-32.png" -colors 32 \) \
+	\( "${TARGET_DIR}/favicon-48.png" -colors 32 \) \
+	"${TARGET_DIR}/favicon.ico"
+rm "${TARGET_DIR}/favicon-16.png" "${TARGET_DIR}/favicon-32.png" "${TARGET_DIR}/favicon-48.png"
